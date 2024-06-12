25186cc313ec6324b17ccc89c26df69909eaabcd
Rehan Nasir <111933400+rehan-nasir@users.noreply.github.com>
2024-06-12 01:51:36 -0400

    WIP JS Updated
    added function for time/date
    clicking email icon now copies email to clipboard

--- script.js
+++ script.js
@@ -5,8 +5,51 @@ function scrollToContact(){
     })
 }
 ScrollReveal().reveal('.scroll-reveal', {
-    delay: 450,
+    delay: 300,
     distance: "30px",
-    origin: "top",
-    reset:true
+    origin: "top"
 });
+function copyEmail() {
+    const img = document.getElementById('copy')
+    const alt = img.getAttribute('alt')
+    navigator.clipboard.writeText(alt)
+    document.getElementById('popup-text').textContent = "Copied!"
+}
+function startTime(){
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    let period = "AM"
+    if (h >= 12) {
+        period = "PM"
+        if (h > 12) {
+            h -= 12
+        }
+    } 
+    else if (h == 0) {
+        h = 12
+    }
+    m = checkTime(m)
+    s = checkTime(s)
+    document.getElementById('clock').innerHTML = h + ":" + m + ":" + s + " " + period
+    setTimeout(startTime, 1000)
+}
+function checkTime(i) {
+    if (i < 10) {
+        i = "0" + i
+    }
+    return i
+}
+function startDate(){
+    var today = new Date();
+    var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    var dayName = days[today.getDay()];
+    var monthName = months[today.getMonth()];
+    var day = today.getDate();
+    var year = today.getFullYear();
+    
+    var formattedDate = dayName + ' ' + monthName + ' ' + day + ', ' + year;
+    document.getElementById('date').textContent = formattedDate;
+}
